beb8d782afa58c39f8900e760dd260965358482e
sprout <smsgb@example.com>
2023-10-17 20:46:51 +0900

    fixed driveby

--- driveby.py
+++ driveby.py
@@ -3,8 +3,8 @@ from flask import Flask, send_file
 app = Flask(__name__)
 
 @app.route('/')
-def get_file(filename):
-    return send_file(f'payload.py')
+def get_file():
+    return send_file('server.py')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
